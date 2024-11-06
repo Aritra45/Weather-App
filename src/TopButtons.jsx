c499dda9ab3a1615f07c66858e4ccf4764fc2ea2
Aritra Deb <aritradeb45@gmail.com>
2024-11-06 18:15:15 +0530

    Update TopButtons.jsx

--- src/TopButtons.jsx
+++ src/TopButtons.jsx
@@ -2,44 +2,56 @@ import React from 'react';
 
 const TopButtons = ({ setQuery }) => {
   const cities = [
-    {
-      id: 1,
-      name: 'London',
-    },
-
-    {
-      id: 2,
-      name: 'Sydney',
-    },
-
-    {
-      id: 3,
-      name: 'Tokyo',
-    },
-
-    {
-      id: 4,
-      name: 'Paris',
-    },
-
-    {
-      id: 5,
-      name: 'Toronto',
-    },
+    { id: 1, name: 'London' },
+    { id: 2, name: 'Sydney' },
+    { id: 3, name: 'Tokyo' },
+    { id: 4, name: 'Paris' },
+    { id: 5, name: 'Toronto' },
   ];
 
   return (
-    <div className="flex items-center justify-around my-6">
-      {cities.map((city) => (
-        <button
-          key={city.id}
-          className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
-          onClick={() => setQuery({ q: city.name })}
-        >
-          {city.name}
-        </button>
-      ))}
+    <div className="my-6">
+      {/* Mobile view (3 buttons in the first row, 2 centered in the second row) */}
+      <div className="block sm:hidden">
+        <div className="flex flex-wrap justify-between mb-4">
+          {cities.slice(0, 3).map((city) => (
+            <button
+              key={city.id}
+              className="text-lg font-medium hover:bg-gray-700/20 px-4 py-2 rounded-md transition ease-in w-1/3 mb-2"
+              onClick={() => setQuery({ q: city.name })}
+            >
+              {city.name}
+            </button>
+          ))}
+        </div>
+
+        <div className="flex justify-center">
+          {cities.slice(3).map((city) => (
+            <button
+              key={city.id}
+              className="text-lg font-medium hover:bg-gray-700/20 px-4 py-2 rounded-md transition ease-in mx-2 w-1/2"
+              onClick={() => setQuery({ q: city.name })}
+            >
+              {city.name}
+            </button>
+          ))}
+        </div>
+      </div>
+
+      {/* Desktop view (All buttons in one row) */}
+      <div className="hidden sm:flex justify-between">
+        {cities.map((city) => (
+          <button
+            key={city.id}
+            className="text-lg font-medium hover:bg-gray-700/20 px-4 py-2 rounded-md transition ease-in"
+            onClick={() => setQuery({ q: city.name })}
+          >
+            {city.name}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
+
 export default TopButtons;

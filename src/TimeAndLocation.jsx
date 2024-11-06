5bb89da2fdc69a999517e27b2960f2d31ce7d060
Aritra Deb <aritradeb45@gmail.com>
2024-11-06 18:14:47 +0530

    Update TimeAndLocation.jsx

--- src/TimeAndLocation.jsx
+++ src/TimeAndLocation.jsx
@@ -4,13 +4,17 @@ const TimeAndLocation = ({
   weather: { formattedLocalTime, name, country },
 }) => {
   return (
-    <div>
-      <div className="flex items-center justify-center my-6">
-        <p className="text-xl font-extralight">{formattedLocalTime}</p>
+    <div className="px-4 md:px-0">
+      <div className="flex items-center justify-center my-4">
+        <p className="text-lg md:text-xl font-extralight text-center">
+          {formattedLocalTime}
+        </p>
       </div>
 
       <div className="flex items-center justify-center my-3">
-        <p className="text-3xl font-medium">{`${name}, ${country}`}</p>
+        <p className="text-2xl md:text-3xl font-medium text-center">
+          {`${name}, ${country}`}
+        </p>
       </div>
     </div>
   );

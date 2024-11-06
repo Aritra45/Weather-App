5904de6d8b9d5fdb64ae863506d5f61aaa2ae7c8
Aritra Deb <aritradeb45@gmail.com>
2024-11-06 18:14:34 +0530

    Update TempAndDetails.jsx

--- src/TempAndDetails.jsx
+++ src/TempAndDetails.jsx
@@ -26,14 +26,12 @@ const TempAndDetails = ({
       title: 'Real Feel',
       value: `${feels_like.toFixed()}°`,
     },
-
     {
       id: 2,
       Icon: BiSolidDropletHalf,
       title: 'Humidity',
       value: `${humidity.toFixed()}%`,
     },
-
     {
       id: 3,
       Icon: FiWind,
@@ -42,49 +40,44 @@ const TempAndDetails = ({
     },
   ];
 
-  const horizontaDetails = [
+  const horizontalDetails = [
     {
       id: 1,
       Icon: GiSunrise,
       title: 'Sunrise',
       value: sunrise,
     },
-
     {
       id: 2,
       Icon: GiSunset,
       title: 'Sunset',
       value: sunset,
     },
-
     {
       id: 3,
       Icon: MdKeyboardArrowUp,
       title: 'High',
-      value: `${temp_max.toFixed()}`,
+      value: `${temp_max.toFixed()}°`,
     },
-
     {
       id: 4,
       Icon: MdKeyboardArrowDown,
       title: 'Low',
-      value: `${temp_min.toFixed()}`,
+      value: `${temp_min.toFixed()}°`,
     },
   ];
 
   return (
-    <div>
+    <div className="px-4 md:px-0">
       <div className="flex items-center justify-center py-6 text-xl text-cyan-300">
-        {' '}
         <p>{details}</p>
       </div>
 
-      <div className="flex flex-row items-center justify-between py-3">
-        <img src={icon} alt="weather icon" className="w-20" />
+      <div className="flex flex-col md:flex-row items-center justify-between py-3 space-y-4 md:space-y-0">
+        <img src={icon} alt="weather icon" className="w-20 mb-4 md:mb-0" />
+        <p className="text-5xl mb-4 md:mb-0">{`${temp.toFixed()}°`}</p>
 
-        <p className="text-5xl">{`${temp.toFixed()}°`}</p>
-
-        <div className="flex flex-col space-y-3 items-start">
+        <div className="flex flex-col space-y-3 items-center md:items-start">
           {verticalDetails.map(({ id, Icon, title, value }) => (
             <div
               key={id}
@@ -96,14 +89,14 @@ const TempAndDetails = ({
           ))}
         </div>
       </div>
-      <div className="flex flex-row items-center justify-center space-x-10 text-sm py-3">
-        {horizontaDetails.map(({ id, Icon, title, value }) => (
-          <div key={id} className="flex flex-row items-center">
-            <Icon size={30} />
 
+      {/* Horizontal details displayed in separate lines on mobile */}
+      <div className="flex flex-col md:flex-row items-center justify-center md:space-x-10 text-sm py-3 gap-4">
+        {horizontalDetails.map(({ id, Icon, title, value }) => (
+          <div key={id} className="flex flex-row items-center justify-center">
+            <Icon size={30} />
             <p className="font-light ml-1">
               {`${title}: `}
-
               <span className="font-medium ml-1">{value}</span>
             </p>
           </div>

fbb23690860e39bb0589c26543782b15001ac09c
Aritra Deb <aritradeb45@gmail.com>
2024-11-08 20:51:24 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,19 +2,37 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="bg-gray-800 text-white mt-50 ">
-      <div className="container mx-auto flex flex-col items-center justify-center">
-        <p className="text-center text-sm mb-2">© 2024 Weather Repoter</p>
-        <p className="text-center text-sm">
-          Developed by{' '}
-          <a
-            href="https://aritradeb-portfolio.netlify.app/"
-            className="text-blue-400 hover:underline"
-            target="_blank"
-          >
-            Aritra Deb
-          </a>
+    <div className="bg-gray-800 text-white mt-10 py-6">
+      <div className="container mx-auto flex flex-col items-center justify-center space-y-4">
+        <p className="text-center font-semibold text-lg">
+          Download Weather Reporter From GitHub
         </p>
+        <a
+          href="https://github.com/Aritra45/Weather-App/releases/download/v1.0/WeatherReporter.apk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="gitlogo.jpg" // Replace this with the actual image URL or path
+            alt="Download app"
+            className="w-32 h-auto mb-2" // Adjust width as needed
+          />
+        </a>
+
+        <div className="text-center text-sm mt-4">
+          <p>© 2024 Weather Reporter</p>
+          <p>
+            Developed by{' '}
+            <a
+              href="https://aritradeb-portfolio.netlify.app/"
+              className="text-blue-400 hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Aritra Deb
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
